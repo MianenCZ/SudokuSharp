d2fc50448f383fbe8cfbe5b27d2abbbe9b142452
Jiří Pelc <jirik.pelc1998@gmail.com>
2021-07-21 18:25:53 +0200

    feature: Update to .NET 5.0

---
+++ SudokuSharpCore/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+using SudokuGame;
+
+namespace SudokuSharp
+{
+    class Program
+    {
+        static void testRuntime()
+        {
+            (string[] task, string[] solution) = SudokuGenerator.GenerateSudoku(Level.Evil);
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var data = task[i * 9 + j];
+                    Console.Write(data != "0" ? data : " ");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(solution[i * 9 + j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            testRuntime();
+            Console.ReadKey();
+        }
+    }
+}
